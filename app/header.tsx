42812c15e392fdbce5704e965f9f25ff90f27030
Sereitepy OR <sereitepy.or@allweb.com.kh>
2025-11-19 17:30:07 +0700

    Worked on Header and Hero Section

---
+++ app/header.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <div className='flex items-center justify-between px-10 py-5 font-semibold bg-pink-800 text-white dark:bg-pink-950'>
+      <p className='text-lg'>Uni Life</p>
+      <div className='flex items-center justify-between gap-5 transition-all duration-700 delay-700 ease-in-out'>
+        <p className='underline-animate'>Universities</p>
+        <p className='underline-animate'>Volunteerings</p>
+        <p className='underline-animate'>Activites</p>
+        <p className='underline-animate'>Courses</p>
+        <p className='underline-animate'>Internships</p>
+      </div>
+      <div>More</div>
+    </div>
+  )
+}
+
+export default Header
